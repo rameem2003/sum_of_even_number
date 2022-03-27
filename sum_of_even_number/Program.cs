8557f2fb2855d7150382a8f5c0180843c48094b9
rameem2003 <rameem2019@gmail.com>
2022-03-27 21:42:22 +0600

    Add project files.

---
+++ sum_of_even_number/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace sum_of_even_number
+{
+    class app
+    {
+        public static void Main()
+        {
+            int a = 2;
+            int sum = 0;
+
+            while (a <= 100)
+            {
+                sum = sum + a;
+                a = a + 2;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
